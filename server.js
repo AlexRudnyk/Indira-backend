89b21a81f7a2ece98e1c80e58f1f798256b36273
AlexRudnyk <alexander.rudnyk@ukr.net>
2023-07-16 10:40:27 +0300

    adds packages and starts server

---
+++ server.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const app = require("./app");
+require("dotenv").config();
+
+const { DB_HOST, PORT = 3030 } = process.env;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() =>
+    app.listen(PORT, async () => {
+      console.log(`Database connection successful. API is on port: ${PORT}`);
+    })
+  )
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
