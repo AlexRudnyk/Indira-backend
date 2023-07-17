39324da6fbb596890c482604db42769bc8d2feff
AlexRudnyk <alexander.rudnyk@ukr.net>
2023-07-17 19:08:48 +0300

    Update getGoods.js

--- controllers/goods/getGoods.js
+++ controllers/goods/getGoods.js
@@ -1,14 +1,10 @@
 const { Good } = require("../../models");
 
 const getGoods = async (req, res) => {
-  const goods = await Good.find();
-  if (goods.length === 0) {
-    res.json({
-      results: "Sorry, there is no news yet",
-    });
-  }
+  const goods = await Good.find({});
 
   res.json(goods);
+
   // const { page = 1, limit = 10 } = req.query;
   // const skip = (page - 1) * limit;
 
